0ed2575461a17cd11a379662c012bf5968f54975
Kenan <kenankzmv@gmail.com>
2017-08-20 14:05:33 +0400

    Update website.py

--- website.py
+++ website.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, redirect, url_for
 import urllib.request
 import json
 from bs4 import BeautifulSoup
